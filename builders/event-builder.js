bad3b2f64717e5d8cfad3481533593c8f8c5cb7b
George Knee <georgeknee@googlemail.com>
2019-10-18 09:50:24 +0100

    Events get a section heading

--- builders/event-builder.js
+++ builders/event-builder.js
@@ -9,7 +9,7 @@ module.exports = {
 
     const builder = [];
 
-    builder.push(`**${i18n.translate("Events")}**`);
+    builder.push(`## ${i18n.translate("Events")}`);
     builder.push("\n");
     builder.push("\n");
     builder.push("```solidity");
